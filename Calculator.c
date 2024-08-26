08dc89b90f9d2cf3ec9fb67ffaadce23d83217ac
Marlin Prapty <marlin.prapty14@gmail.com>
2024-08-26 07:48:01 -0700

    Create Simple Calculator.c

---
+++ Calculator.c
@@ -0,0 +1,45 @@
+// Program to create a simple calculator
+#include <stdio.h>
+
+int main()
+{
+  char operation;
+  double n1, n2;
+
+  printf("Enter an operation (+, -, *, /): ");
+  scanf("%c", &operation);
+  printf("Enter two operands : ");
+  scanf("%lf  %lf", &n1, &n2);
+
+
+  switch (operation)
+  {
+
+  case '+':
+    printf("%.1lf + %.1lf = %.1lf", n1,n2, n1+n2);
+    break;
+
+  case '-':
+    printf("%.1lf - %.1lf = %.1lf", n1,n2, n1-n2);
+    break;
+
+    case '*':
+    printf("%.1lf * %.1lf = %.1lf", n1,n2, n1*n2);
+    break;
+
+    case '/':
+    printf("%.1lf / %.1lf = %.1lf", n1,n2, n1/n2);
+    break;
+
+    //operator doesn't match any case constant +, -, *, /
+
+  
+  default:
+    printf("Error! operator is not correct");
+
+    break;
+  }
+
+    
+    return 0;
+}
